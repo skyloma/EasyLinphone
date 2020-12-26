d17853cfa3603ba4feedaf412c3a857538e63df8
lomasky <271341794@qq.com>
2020-12-26 23:44:29 +0800

    #androidX

--- easylinphone/src/main/java/com/xuchongyang/easyphone/service/LinphoneService.java
+++ easylinphone/src/main/java/com/xuchongyang/easyphone/service/LinphoneService.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.SystemClock;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.xuchongyang.easyphone.callback.PhoneCallback;
